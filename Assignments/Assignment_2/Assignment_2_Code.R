2bcd1267ecaa000e81bd094da9e656515328d938
SarahGhiaie <11050951@uvu.edu>
2024-11-26 23:51:29 -0700

    Assignment_3

---
+++ Assignments/Assignment_2/Assignment_2_Code.R
@@ -0,0 +1,26 @@
+#view current working directory
+getwd()
+#set working directory to '/Data'
+setwd("/Users/sarah/Desktop/Data_Course_SYLVESTER/Data")
+#list all of the .csv files found in wd
+list.files(pattern=".csv")
+#store the list in an object named "csv_files"
+csv_files <- list.files(pattern = ".csv")
+#find how many files match the description
+length(csv_files)
+#open wingspan_vs_mass file and assign data to object called "df"
+df <- read.csv("wingspan_vs_mass.csv")
+#inspect first 5 lines
+head(df)
+#find any files in Data directory beginning with "b"
+list.files(pattern = "^b", recursive = TRUE)
+#display first line of each "b" file
+b_files <- (list.files(pattern = "^b", recursive = TRUE))
+for(i in b_files){
+  print(readLines(i, n=1))
+}
+#do the same for all ".csv" files
+for(j in csv_files){
+  print(readLines(j,n=1))
+}
+
--- Assignments/Assignment_3/Assignment_3.R
+++ Assignments/Assignment_3/Assignment_3.R
@@ -154,26 +154,32 @@ dbinom(50,100,.5) # probability of getting exactly 50 heads out of 100 coin flip
 # YOUR REMAINING HOMEWORK ASSIGNMENT (Fill in with code) ####
 
 # 1.  Get a subset of the "iris" data frame where it's just even-numbered rows
+v1 <- as.vector(iris$Sepal.Length)
+v2 <- seq(2,150,2)
 
-seq(2,150,2) # here's the code to get a list of the even numbers between 2 and 150
-
-
+even_num_rows <- v1[v2]
 
 # 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class
-
+iris_chr <- as.character(iris[,1:5])
+class(iris_chr)
 
 
 # 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width
-
+v3 <- as.vector(iris$Sepal.Length)
+v4 <- as.vector(iris$Sepal.Width)
+Sepal.Area <- v3*v4
 
 
 # 4.  Add Sepal.Area to the iris data frame as a new column
+iris$Sepal.Area <- Sepal.Area
 
 
 
 # 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 
       # (name it big_area_iris)
+v6 <- as.vector(iris$Sepal.Area>20)
 
+big_area_iris<- data.frame(iris[v6==TRUE,1:6])
 
 
 # 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) 
